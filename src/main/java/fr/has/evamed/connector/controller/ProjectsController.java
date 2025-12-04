121839278fd65c72e55068e302c9cf7351d8b3a6
Ghassen Khalil Ati <ghassen1khalil@gmail.com>
2025-12-05 00:36:52 +0100

    Add project service and wire controller methods

--- src/main/java/fr/has/evamed/connector/controller/ProjectsController.java
+++ src/main/java/fr/has/evamed/connector/controller/ProjectsController.java
@@ -3,20 +3,30 @@ package fr.has.evamed.connector.controller;
 import fr.has.evamed.connector.domain.PaginatedProjectsDto;
 import fr.has.evamed.connector.domain.ProjectDto;
 import fr.has.evamed.connector.rest.api.ProjectsApi;
-import org.springframework.http.HttpStatus;
+import fr.has.evamed.connector.service.ProjectService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class ProjectsController implements ProjectsApi {
 
+    private final ProjectService projectService;
+
+    public ProjectsController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
     @Override
     public ResponseEntity<PaginatedProjectsDto> listProjects(Integer offset, Integer limit) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+        PaginatedProjectsDto projects = projectService.listProjects(offset, limit);
+        return ResponseEntity.status(OK).body(projects);
     }
 
     @Override
     public ResponseEntity<ProjectDto> getProjectById(String projectId) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+        ProjectDto project = projectService.getProjectById(projectId);
+        return ResponseEntity.status(OK).body(project);
     }
 }
---
+++ src/main/java/fr/has/evamed/connector/service/ProjectService.java
@@ -0,0 +1,17 @@
+package fr.has.evamed.connector.service;
+
+import fr.has.evamed.connector.domain.PaginatedProjectsDto;
+import fr.has.evamed.connector.domain.ProjectDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    public PaginatedProjectsDto listProjects(Integer offset, Integer limit) {
+        return null;
+    }
+
+    public ProjectDto getProjectById(String projectId) {
+        return null;
+    }
+}
