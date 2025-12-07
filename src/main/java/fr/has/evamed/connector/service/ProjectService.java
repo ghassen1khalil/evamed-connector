449f3a68c51cf08117968ccb33d71e08e982c78e
Ghassen Khalil Ati <g.khalilati@has-sante.fr>
2025-12-08 00:07:01 +0100

    fix(jooq generation): added necessary deps and config in pom.xml for jOOQ code generation & initiated listProjects service method

--- src/main/java/fr/has/evamed/connector/service/ProjectService.java
+++ src/main/java/fr/has/evamed/connector/service/ProjectService.java
@@ -2,12 +2,34 @@ package fr.has.evamed.connector.service;
 
 import fr.has.evamed.connector.domain.PaginatedProjectsDto;
 import fr.has.evamed.connector.domain.ProjectDto;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
 @Service
+@Slf4j
 public class ProjectService {
 
     public PaginatedProjectsDto listProjects(Integer offset, Integer limit) {
+        try {
+            //TODO this is a WIP
+            String userName = "evamed";
+            String password = "evamed";
+            String url = "jdbc:postgresql://10.10.200.15:5432/evamed";
+            Connection conn = DriverManager.getConnection(url, userName, password);
+            DSLContext context = DSL.using(conn, SQLDialect.POSTGRES);
+            Result<Record> dossiers = context.select().from("dossier").fetch();
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
         return null;
     }
 
