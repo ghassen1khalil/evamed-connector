3b5ba0ae3fdddbedc1f61f26a21d33bf7376d9cb
Ghassen Khalil Ati <g.khalilati@has-sante.fr>
2025-12-08 20:21:20 +0100

    WIP

--- src/main/java/fr/has/evamed/connector/controller/ProjectsController.java
+++ src/main/java/fr/has/evamed/connector/controller/ProjectsController.java
@@ -1,6 +1,7 @@
 package fr.has.evamed.connector.controller;
 
-import fr.has.evamed.connector.domain.PaginatedProjectsDto;
+//import fr.has.evamed.connector.domain.PaginatedProjectsDto;
+import fr.has.evamed.connector.domain.PaginatedProjectResponseDto;
 import fr.has.evamed.connector.domain.ProjectDto;
 import fr.has.evamed.connector.rest.api.ProjectsApi;
 import org.springframework.http.HttpStatus;
@@ -10,13 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProjectsController implements ProjectsApi {
 
-    @Override
-    public ResponseEntity<PaginatedProjectsDto> listProjects(Integer offset, Integer limit) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
-    }
-
-    @Override
-    public ResponseEntity<ProjectDto> getProjectById(String projectId) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
-    }
+    
 }
