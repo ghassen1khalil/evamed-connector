944ff8d6e282e116ae8748f4e50cbc7b74ac401a
Ghassen Khalil Ati <g.khalilati@has-sante.fr>
2025-12-08 21:27:48 +0100

    feat(projects): added 2 endpoints for returning projects by manager and assistant

--- src/main/java/fr/has/evamed/connector/controller/ProjectsController.java
+++ src/main/java/fr/has/evamed/connector/controller/ProjectsController.java
@@ -1,22 +1,42 @@
 package fr.has.evamed.connector.controller;
 
-import fr.has.evamed.connector.domain.PaginatedProjectsDto;
+import fr.has.evamed.connector.domain.PaginatedProjectResponseDto;
 import fr.has.evamed.connector.domain.ProjectDto;
 import fr.has.evamed.connector.rest.api.ProjectsApi;
-import org.springframework.http.HttpStatus;
+import fr.has.evamed.connector.service.ProjectService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProjectsController implements ProjectsApi {
 
-    @Override
-    public ResponseEntity<PaginatedProjectsDto> listProjects(Integer offset, Integer limit) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    private final ProjectService projectService;
+
+    public ProjectsController(ProjectService projectService) {
+        this.projectService = projectService;
     }
 
     @Override
     public ResponseEntity<ProjectDto> getProjectById(String projectId) {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+        return ProjectsApi.super.getProjectById(projectId);
+    }
+
+    @Override
+    public ResponseEntity<Map<String, List<ProjectDto>>> getProjectsForAssistant() {
+        return ProjectsApi.super.getProjectsForAssistant();
+    }
+
+    @Override
+    public ResponseEntity<Map<String, List<ProjectDto>>> getProjectsForManager() {
+        return ProjectsApi.super.getProjectsForManager();
+    }
+
+    @Override
+    public ResponseEntity<PaginatedProjectResponseDto> getProjects(Integer offset, Integer limit, String projectManagerId, String managementAssistantId, String sortBy, String sortDirection) {
+        projectService.getProjects(offset, limit);
+        return ProjectsApi.super.getProjects(offset, limit, projectManagerId, managementAssistantId, sortBy, sortDirection);
     }
 }
